ce5b66a8aebe2d608fef3a019c8132d77d05cf20
tatsukoni_macPro2 <tatsukoni_macpro2@tatsukoni-macPro2noMacBook-Pro.local>
2020-09-13 11:51:35 +0900

    first commit

---
+++ bin/build.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+HOME_PATH='/Users/tatsukoni_macpro2/Desktop/go_prac'
+BUILD_FILE=$1
+EXEC_FILE=`echo $BUILD_FILE | sed s/.go//`
+
+# move to build file path
+PATH=`find $HOME_PATH -name $BUILD_FILE | sed s/$BUILD_FILE//`
+cd $PATH
+
+# go build
+go build
+
+# go exuc
+./$EXEC_FILE
